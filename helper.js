d874a715a81b126ff70ba5dc84e136abb3918749
Bhanu pratap singh <pbhanu314@gmail.com>
2021-12-07 22:18:35 +0530

    all database file created and one helper file

---
+++ helper.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+var content = [
+    {
+        "": [],
+        "01": [],
+        "02": [],
+        "03": [],
+        "04": [],
+        "05": [],
+        "06": [],
+        "07": [],
+        "08": [],
+        "09": [],
+        "10": [],
+        "11": [],
+        "12": []
+    }
+];
+
+// for (var i = 1877; i < 2020; i++) {
+//     fs.writeFile('database/' + i + '.json', JSON.stringify(content), function (err) {
+//         if (err) throw err;
+//         console.log('Saved!');
+//     });
+// }
+
--- index.js
+++ index.js
@@ -1,8 +1,97 @@
 var fs = require('fs');
-const first = require('./database/1875.json');
-// console.log(json);
+// const first = require('./database/1875.json');
 
+// console.log(first[0]["04"][0]["10"][0].title);
 
-fs.readFile("./database/1875.json", 'utf-8', (err, jsonString) => {
-    console.log(jsonString);
-});
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+console.log(dd, mm, yyyy);
+
+const randomYear = () => {
+    return Math.floor(Math.random() * ((yyyy - 1) - 1875)) + 1875;
+};
+const randomDD = () => {
+    return Math.floor(Math.random() * (28 - 1)) + 1;
+};
+const randomMM = () => {
+    return Math.floor(Math.random() * (12 - 1)) + 1;
+};
+
+// console.log(randomYear());
+// console.log(randomDD());
+// console.log(randomMM());
+
+// const getQuoteByYear = (start, end) => {
+//     return array
+//         .filter(i => i.year >= start && i.year <= end)
+//         .sort((a, b) => (a.year > b.year ? 1 : b.year > a.year ? -1 : 0));
+// };
+
+const getOnlyOneRandomEvent = () => {
+    let randomY = randomYear().toString();
+    let randomDay = randomDD().toString();
+    let randomMonth = randomMM().toString();
+    let year = require('./database/' + randomY + '.json');
+    // console.log(randomY, randomDay, randomMonth);
+    // console.log(year[0][randomMonth]);
+    if (year[0][randomMonth] == undefined) {
+        console.log("undefined");
+        return undefined;
+    }
+    if (year[0][randomMonth][0][randomDay] == undefined) {
+        console.log("undefined Day");
+        return undefined;
+    }
+    return year[0][randomMonth][0][randomDay][0];
+};
+console.log(getOnlyOneRandomEvent());
+
+const getSomeRandom = count => {
+    let randomQuotesArray = [];
+    let randomQuotesSet = new Set(); // to prevent duplicate quotes
+    while (randomQuotesArray.length < count) {
+        let quote = array[randomNum()];
+        if (!randomQuotesSet.has(quote)) {
+            randomQuotesArray.push(quote);
+        }
+    }
+    return randomQuotesArray;
+};
+
+// const getQuotesByObject = (quote, obj) => {
+//     let resultArray = [];
+//     quote = quote.toLowerCase();
+//     object = obj;
+//     array.forEach(item => {
+//         item[object] = item[object].toLowerCase();
+//         if (item[object] && item[object].includes(quote)) {
+//             resultArray.push(item);
+//         }
+//     });
+
+//     return resultArray;
+// };
+
+// const getQuotesByMovie = quote => {
+//     return getQuotesByObject(quote, "movie");
+// };
+
+// const getQuotesByType = quote => {
+//     return getQuotesByObject(quote, "type");
+// };
+
+// const getAll = () => {
+//     return array;
+// };
+
+// module.exports = {
+//     getAll,
+//     getRandomQuote,
+//     getSomeRandom,
+//     getQuoteByYear,
+//     getQuotesByMovie,
+//     getQuotesByType
+// };
